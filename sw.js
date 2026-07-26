a76b0625648e3cfd4d752df019d9aa07a4d83196
Jasmin Chacko <jasmin.chacko16@gmail.com>
2026-07-26 20:10:48 +0400

    Show custom onboarding moments

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'bloom-shell-v19';
+const CACHE = 'bloom-shell-v20';
 const APP_SHELL = ['./', './index.html', './manifest.webmanifest', './icon.svg'];
 
 self.addEventListener('install', (event) => {
