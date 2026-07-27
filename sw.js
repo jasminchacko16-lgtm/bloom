673beae182f0eeb5b0caad7fcb5d8d47eaffe680
Jasmin Chacko <jasmin.chacko16@gmail.com>
2026-07-27 18:15:07 +0400

    Add parent-friendly reward board guide

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'bloom-shell-v20';
+const CACHE = 'bloom-shell-v21';
 const APP_SHELL = ['./', './index.html', './manifest.webmanifest', './icon.svg'];
 
 self.addEventListener('install', (event) => {
